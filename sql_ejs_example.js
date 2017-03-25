f50c732ac4b57a6e12b2072fac801d3c7b1a3a6d
Gavin Roy <Gavin@Gavins-MacBook-Pro.local>
2017-03-25 14:00:09 -0700

    work...

--- sql_ejs_example.js
+++ sql_ejs_example.js
@@ -45,6 +45,6 @@ app.get('/about', function(req, res) {
     });
 });
 
-app.listen(8000, function() {
-    console.log('Example app listening on port 8000!')
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!')
 })
